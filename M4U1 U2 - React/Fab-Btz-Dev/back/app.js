6d0a2f8fedac0a06da134583b93df7cdcf25fd5d
Fabian Benitez <fabianbenitez25@gmail.com>
2022-10-20 13:39:22 -0300

    M5U3 - BackEnd MySQL

--- M4U1 U2 - React/Fab-Btz-Dev/back/app.js
+++ M4U1 U2 - React/Fab-Btz-Dev/back/app.js
@@ -4,10 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//llamamos al arvhico .env
+require('dotenv').config();
+var pool = require('./models/db');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 // GENERAMOS VARIABLE CON RUTA PARA LOGIN
 var loginRouter = require('./routes/admin/login');
+const { createPool } = require('mysql');
 
 var app = express();
 
@@ -23,10 +28,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//-----------------------------------------------------
 //mod
 app.use('/admin/login', loginRouter);
 
+// Consulta select
+/*pool.query('SELECT * FROM empleados').then(function (resultados){
+  console.log(resultados);
+});*/
 
+//insert
+
+// var obj = {
+//   nombre: 'Fabian',
+//   apellido: 'Benitez',
+//   trabajo: 'Programador',
+//   edad: 35,
+//   salario: 120000,
+//   mail: 'fabianbenitez25@gmail.com'
+// }
+// pool.query('INSERT INTO empleados SET ?', 
+// [obj]).then(function(resultados) {
+//   console.log(resultados)
+// });
+
+//update
+/*var id = 23;
+var obj = {
+  nombre: 'Victor Fabian',
+  apellido: 'Benitez Ocampo'
+}
+pool.query('UPDATE empleados SET ? WHERE id_emp=?', [obj, id]).then
+(function (resultados){
+  console.log(resultados);
+});*/
+
+//delete
+
+// var id = 25;
+// pool.query('DELETE FROM empleados WHERE id_emp=?', [id]).then
+// (function (resultados) {
+//   console.log(`Fue eliminado el usuario ${resultados, id}`);
+// });
+
+//-----------------------------------------------------
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
